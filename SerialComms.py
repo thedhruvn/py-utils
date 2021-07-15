3a3d37984d65af4a4d951a65c999946909a06d31
thedhruvn <thedhruvn@gmail.com>
2021-07-15 15:35:16 -0500

    Add function to list serial ports

--- SerialComms.py
+++ SerialComms.py
@@ -29,6 +29,38 @@ class StdBuffer(SerialBuffer):
     def get_text(self, beg=0, end=1):
         return '\n'.join(self.msgBuffer[beg:end])
 
+import sys
+import glob
+
+
+def sys_serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
 
 class SerialComm(ColorLogBase):
     def __init__(self, port='/dev/ttyUSB0', baud=38400):
