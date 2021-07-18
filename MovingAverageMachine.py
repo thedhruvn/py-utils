6326b16e7d519d10c6c8f01d794cd3d2abaf03a0
thedhruvn <thedhruvn@gmail.com>
2021-07-18 16:02:09 -0500

    Variable threshold and window sizes for limit switches

--- MovingAverageMachine.py
+++ MovingAverageMachine.py
@@ -10,9 +10,25 @@ class MovingAverageNumber(ColorLogBase):
 		self.buffer = []
 		self.window = window
 		self.seed = seed
+		self.threshold = 0.5
+		self.__initialize_buffer()
+
+
 		
+	def __initialize_buffer(self):
 		for i in range(0, self.window):
 			self.buffer.append(self.seed)
+
+	def update_window(self, new_window):
+		self.window = new_window
+		self.__initialize_buffer()
+
+	def update_seed(self, new_seed):
+		self.seed = new_seed
+		self.__initialize_buffer()
+
+	def update_threshold(self, new_threshold):
+		self.threshold = new_threshold
 
 	def add(self, value):
 		"""
@@ -30,7 +46,7 @@ class MovingAverageNumber(ColorLogBase):
 	def get_average(self, rounding=None):
 		return self.get_avg(rounding)
 
-	def get_bool_average(self, threshold=0.5, rounding=None):
+	def get_bool_average(self, threshold=self.threshold, rounding=None):
 		"""
 		If Moving Average >= threshold, return True
 		"""
